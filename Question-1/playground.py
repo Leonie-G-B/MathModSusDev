8fdd302581168aaab9b45773da7aea32c4dee67d
Leonie Bottomley <leoniegracebottomley@gmail.com>
2026-03-06 15:19:35 +0000

    Question 1 initial commit
    Mostly just the stuff provided by the uni

---
+++ Question-1/playground.py
@@ -0,0 +1,11 @@
+
+# Just having a play around with the practice codes and understanding the logic 
+
+
+# we know lambda can VARY between 8 and 16 - how do we want to approach this information? 
+# eiither way, lambda needs to be a value that can easily be varied (and iterated upon?)
+
+# to iterate, should the entire process be repeatable and results independent of number of trials? 
+
+
+
